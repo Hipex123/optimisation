fa81bd8b0183ddb06ca15bb8b83e465157ba2080
HIpex <117768734+Hipex123@users.noreply.github.com>
2025-02-02 14:09:14 +0100

    Added indexing
    still fixing constructors

--- include/Array2D.hpp
+++ include/Array2D.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <initializer_list>
+#include <iostream>
+#include <cstring>
+#include <utility>
+#include <stdexcept>
 
 template<typename T>
 class Array2D{
@@ -8,7 +11,7 @@ public:
 	Array2D()=default;
 
 	Array2D(const std::initializer_list<std::initializer_list<T>> &others);
-	Array2D(const size_t row, const size_t column); // TODO
+	Array2D(const size_t row, const size_t column);
 
 	Array2D(const Array2D<T> &other);
 	Array2D(Array2D<T> &&other);
@@ -16,14 +19,110 @@ public:
 	Array2D<T> &operator=(const Array2D<T> &other) noexcept;
 	Array2D<T> &operator=(Array2D<T> &&other) noexcept;
 
-	T *operator[](size_t row); // TODO
-	const T *operator[](size_t row); // TODO
+	T *operator[](size_t row);
+	const T *operator[](size_t row) const;
 
-	~Array2D(); // TODO
+	~Array2D();
 
 
 private:
 	T *m_data;
-	size_t row;
-	size_t column;
+	size_t m_row;
+	size_t m_column;
 };
+
+template<typename T>
+Array2D<T>::Array2D(const std::initializer_list<std::initializer_list<T>> &other)
+:	m_row(other.size()), m_column(other.begin().size())
+{
+        delete[] m_data;
+        m_data = new T[m_row*m_column];
+
+        for (int i=0; i<m_row; i++) {
+                for (int ii=0; ii<m_column; ii++) {
+                        m_data[i*m_column+ii] = (*std::next(other.begin(), i))[ii];
+                }
+        }
+}
+
+template<typename T>
+Array2D<T>::Array2D(const size_t row, const size_t column)
+:	m_row(row), m_column(column), m_data(new T[row*column])
+{}
+
+template<typename T>
+Array2D<T>::Array2D(const Array2D<T> &other)
+:	m_row(other.m_row), m_column(other.m_column)
+{
+        delete[] m_data;
+        m_data = new T[m_row*m_column];
+
+        std::memcpy(m_data, other.m_data, sizeof(T)*m_row*m_column);
+}
+
+template<typename T>
+Array2D<T>::Array2D(Array2D<T> &&other)
+: m_data(std::move(other.m_data))
+{
+        other.m_row = 0;
+        other.m_column = 0;
+        other.m_data = nullptr;
+}
+
+template<typename T>
+Array2D<T> &Array2D<T>::operator=(const Array2D<T> &other) noexcept
+{
+        if (this == &other)
+        	return *this;
+
+        m_row = other.m_row;
+        m_column = other.m_column;
+
+        delete[] m_data;
+        m_data = new T[m_row*m_column];
+
+        std::memcpy(m_data, other.m_data, sizeof(T)*m_row*m_column);
+
+        return *this;
+}
+
+template<typename T>
+Array2D<T> &Array2D<T>::operator=(Array2D<T> &&other) noexcept
+{
+        delete[] m_data;
+        m_data = std::move(other.m_data);
+
+        other.m_row = 0;
+        other.m_column = 0;
+        other.m_data = nullptr;
+
+        return *this;
+}
+
+template<typename T>
+T* Array2D<T>::operator[](size_t row)
+{
+        if (row >= m_row) {
+                throw std::out_of_range("Row index out of bounds");
+        }
+
+        return &m_data[row*m_column];
+}
+
+template<typename T>
+const T* Array2D<T>::operator[](size_t row) const
+{
+        if (row >= m_row) {
+                throw std::out_of_range("Row index out of bounds");
+        }
+
+        return &m_data[row*m_column];
+}
+
+template<typename T>
+Array2D<T>::~Array2D()
+{
+	delete[] m_data;
+	m_row = 0;
+	m_column = 0;
+}
--- src/_test.cpp
+++ src/_test.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <chrono>
-#include <initializer_list>
 #include "Array2D.hpp"
 
 int main()
 {
 	auto start = std::chrono::high_resolution_clock::now();
-	Array2D<int> a;
-	std::initializer_list<int> b;
+	Array2D<int> a(100, 100);
 
 	for (int i=0; i<100; i++) {
-		for (int ii; ii<100; ii++) {
-			b[i][ii] = i+ii*100;
+		for (int ii=0; ii<100; ii++) {
+			a[i][ii] = ii;
 		}
 	}
-
-	std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start).count() << "\n";
+	try {
+		Array2D<int> b(a);
+	}
+	catch (const e){
+		std::cout<<"tEst\n";
+	}
+	std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start).count() <<"\n";
 	return 0;
 }
